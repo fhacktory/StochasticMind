2d7ae97e3681bd4f88b9392915651390310d6e7a
Alexandre Ramchurn <alex_ram@hotmail.fr>
2014-10-18 17:34:05 +0200

    Buttons added with colors - Background textures.

---
+++ Assets/Scripts/MainBattle/HUD/ActionsMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionsMenu : MonoBehaviour
+{
+    public Font textFont;
+
+	void Start ()
+	{
+	
+	}
+	
+	void Update ()
+	{
+	
+	}
+
+	void OnGUI()
+	{
+		GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
+		
+		buttonStyle.fontSize = (int)(Screen.width * 0.027f);
+		buttonStyle.font = textFont;
+		buttonStyle.hover.textColor = new Color(0.4f, 0.0f, 0.1f);
+        buttonStyle.normal.textColor = new Color(0.8f, 0.4f, 0.1f);
+
+        if (GUI.Button(new Rect(Screen.width * 0.1f, Screen.height * 0.85f,
+                            Screen.width * 0.2f, Screen.height * 0.1f), "Attack", buttonStyle))
+            //Application.LoadLevel(0);
+
+        buttonStyle.normal.textColor = new Color(0.1f, 0.4f, 0.5f);
+
+        GUI.Button(new Rect(Screen.width * 0.4f, Screen.height * 0.85f,
+                            Screen.width * 0.2f, Screen.height * 0.1f), "Defense", buttonStyle);
+
+        buttonStyle.normal.textColor = new Color(0.8f, 0.8f, 0.0f);
+
+        GUI.Button(new Rect(Screen.width * 0.7f, Screen.height * 0.85f,
+                            Screen.width * 0.2f, Screen.height * 0.1f), "Item", buttonStyle);
+	}
+}
---
+++ Assets/Scripts/MainBattle/HUD/NameDisplay.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NameDisplay : MonoBehaviour
+{
+    public Font     textFont;
+
+    private EnemyAttributes     enemyAttributes;
+    private PlayerAttributes    playerAttributes;
+
+	void Start ()
+    {
+        enemyAttributes = GameObject.FindGameObjectWithTag("Enemy").GetComponent<EnemyAttributes>();
+        playerAttributes = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAttributes>();
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    void OnGUI()
+    {
+        GUIStyle labelStyle = new GUIStyle(GUI.skin.label);
+
+        labelStyle.font = textFont;
+        labelStyle.fontSize = (int)(Screen.width * 0.04f);
+        labelStyle.normal.textColor = new Color(0.4f, 0.1f, 0.6f);
+
+        GUI.Label(new Rect(Screen.width * 0.26f, Screen.height * 0.02f,
+            Screen.width * 0.25f, Screen.height * 0.25f), enemyAttributes.name, labelStyle);
+
+        labelStyle.normal.textColor = new Color(0.1f, 0.4f, 0.0f);
+
+        GUI.Label(new Rect(Screen.width * 0.77f, Screen.height * 0.5f,
+            Screen.width * 0.25f, Screen.height * 0.25f), playerAttributes.name, labelStyle);
+    }
+}
