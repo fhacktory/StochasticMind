4fabf34cedc82b7d2b65718f774c6857c5b03a2f
tanguy morvant <t.morvant@student.isartdigital.com>
2014-10-18 18:43:58 +0200

    Shoot em up mini game :   - The player can move and shoot   - Enemies can move and shoot   - Enemies and player can be killed

---
+++ Assets/Scripts/ShootEmUp/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+
+    [SerializeField]
+    private float speed;
+    private Transform trans;
+
+
+	// Use this for initialization
+	void Start () {
+        trans = transform;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        trans.Translate(trans.up * speed * Time.deltaTime, Space.World);
+    }
+
+    void OnTriggerEnter(Collider hit)
+    {
+        Debug.Log(hit.name);
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter(Collision hit)
+    {
+        Debug.Log(hit.collider.name);
+        Destroy(gameObject);
+    }
+}
