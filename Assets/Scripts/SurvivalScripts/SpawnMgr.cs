f99bc42b81e42034224550f02a54b31a1a4e6076
j.seys <j.seys@student.isartdigital.com>
2014-10-18 20:25:53 +0200

    Completed survival scene for basic mechanics

--- Assets/Scripts/SurvivalScripts/SpawnMgr.cs
+++ Assets/Scripts/SurvivalScripts/SpawnMgr.cs
@@ -4,4 +4,12 @@ using System.Collections;
 public class SpawnMgr : MonoBehaviour {
 
     public GameObject[] Spawners;
+    public GameObject   Enemy;
+    public GameObject   Button;
+
+    void Start()
+    {
+        Enemy.transform.position = Spawners[Random.Range(0, Spawners.Length)].transform.position;
+        Button.transform.position = Spawners[Random.Range(0, Spawners.Length)].transform.position;
+    }
 }
