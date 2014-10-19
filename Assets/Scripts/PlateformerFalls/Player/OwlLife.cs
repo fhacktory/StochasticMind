ed0dd42ce0bedf1b460ce2a27fafdc3cc1c0512d
Alexandre Ramchurn <alex_ram@hotmail.fr>
2014-10-19 08:37:44 +0200

    Added PlateformerFalls

---
+++ Assets/Scripts/PlateformerFalls/Player/OwlLife.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OwlLife : MonoBehaviour
+{
+    public int  life;
+    public bool hasWon;
+
+	void Start ()
+    {
+        life = 1;
+	}
+	
+	void Update ()
+    {
+        //if (0 == life)
+          //  ;
+	}
+}
---
+++ Assets/Scripts/PlateformerFalls/World/DoorTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTrigger : MonoBehaviour
+{
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter (Collider hit)
+    {
+        if ("Player" ==  hit.tag)
+        {
+            hit.GetComponent<OwlLife>().hasWon = true;
+        }
+    }
+}
